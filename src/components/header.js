a0d79f72786481d84451936d689bb7e44c7c9b5f
junhan <ngjunhan98@gmail.com>
2025-05-05 20:15:12 +0800

    add header alignment

--- src/components/header.js
+++ src/components/header.js
@@ -8,21 +8,25 @@ const Header = ({ onLogout, onDashboard, sessionToken }) => {
         <Typography
           variant="h6"
           sx={{ flexGrow: 1, fontFamily: "Montserrat, sans-serif" }}
-          onClick={onDashboard}
         >
-          🦆 Applycation Dashboard
+          Welcome back, {sessionToken.username}
         </Typography>
         <Typography
           variant="h6"
           sx={{ flexGrow: 1, fontFamily: "Montserrat, sans-serif" }}
+          onClick={onDashboard}
         >
-          {sessionToken.username} | {sessionToken.role}
+          🦆 Applycation Dashboard
         </Typography>
         <Button
           color="inherit"
           onClick={onLogout}
           sx={{
             fontFamily: "Montserrat, sans-serif",
+            backgroundColor: "#4F959D", 
+            "&:hover": {
+              backgroundColor: "#98D2C0",
+            },
           }}
         >
           Logout
